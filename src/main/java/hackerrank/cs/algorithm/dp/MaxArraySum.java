cb5b2b2d245aefd654c1f6d0e645e9b69291694b
junbeom.bae <junbeom.bae@worksmobile.com>
2019-04-21 16:06:19 +0900

    [20190421] algorithm

--- src/main/java/hackerrank/cs/algorithm/dp/MaxArraySum.java
+++ src/main/java/hackerrank/cs/algorithm/dp/MaxArraySum.java
@@ -3,54 +3,50 @@ package hackerrank.cs.algorithm.dp;
 import java.util.*;
 
 /**
- * 최초 풀이.. 절반정도 Accept..
- *
- * 풀이는 맞으나.. StackOverflow 걸리는듯..
  */
 public class MaxArraySum {
-	static int n;
-	static int [] arr;
-	static long [] cache;
-
 	public static void main(String[] args) {
-
 		Scanner scanner = new Scanner(System.in);
 
-		n = scanner.nextInt();
+		int n = scanner.nextInt();
 
-		arr = new int[n];
-		cache = new long[n];
-
-		Arrays.fill(cache, Long.MIN_VALUE);
+		long [] arr = new long[n];
 
 		for (int i = 0; i < n; i++) {
-			arr[i] = scanner.nextInt();
+			arr[i] = scanner.nextLong();
 		}
 
-		long max = Long.MIN_VALUE;
+		DP [] dp = new DP[n];
 
-		for (int i = 0; i < n - 2; i++) {
-			max = Math.max(max, solve(i));
+		dp[0] = new DP(arr[0], arr[0]);
+		dp[1] = new DP(Math.max(arr[0], arr[1]), arr[1]);
+
+		long ret = Long.MIN_VALUE;
+
+		for (int i = 2; i < n; i++) {
+			// 현재 위치의 최대값은 아래 세가지 조건을 비교하면 된다.
+			// 1. 그냥 자기 자신
+			// 2. i-2 번째 가 가지고 있는 최대값 + 자기 자신 (maxHere)
+			// 3. 처음 ~ i-2 번째까지 가지고 있는 최대값 (maxSoFar)
+			long max = Math.max(arr[i] + dp[i-2].maxSoFar, Math.max(arr[i], arr[i] + dp[i-2].maxHere));
+
+			// 그리고 maxSoFar 의 갱신은 바로 이전의 최대값과 비교해주어야 한다.
+			dp[i] = new DP(Math.max(max, dp[i-1].maxSoFar), max);
+
+			ret = Math.max(ret, max);
 		}
 
-		System.out.println(max);
+		System.out.println(ret);
 	}
 
-	static long solve(int i) {
-		if (i == n-1 || i == n-2) {
-			return arr[i];
+	static class DP {
+		long maxSoFar;
+		long maxHere;
+
+		DP(long maxSoFar, long maxHere) {
+			this.maxSoFar = maxSoFar;
+			this.maxHere = maxHere;
 		}
-
-		if (cache[i] != Long.MIN_VALUE) {
-			return cache[i];
-		}
-
-		long max = Long.MIN_VALUE;
-
-		for (int j = i+2; j < n; j++) {
-			max = Math.max(max, Math.max(arr[i], arr[i] + solve(j)));
-		}
-
-		return cache[i] = max;
 	}
+
 }
--- src/main/java/hackerrank/mathematics/fundamentals/BestDivisor.java
+++
@@ -1,7 +0,0 @@
-package hackerrank.mathematics.fundamentals;
-
-// 최선의 약수찾기
-// https://www.hackerrank.com/challenges/best-divisor/problem
-public class BestDivisor {
-
-}
