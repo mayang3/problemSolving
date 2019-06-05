0e882a897ccb309dda990f71757ac013b964d21e
junbeom.bae <junbeom.bae@worksmobile.com>
2019-06-05 23:02:58 +0900

    [20190605] algorithm

--- src/main/java/hackerrank/cs/dataStructure/queue/CastleOnTheGrid.java
+++ src/main/java/hackerrank/cs/dataStructure/queue/CastleOnTheGrid.java
@@ -1,218 +1,101 @@
 package hackerrank.cs.dataStructure.queue;
 
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 /**
- * You are given a square grid with some cells open (.) and some blocked (X).
- *
- * Your playing piece can move along any row or column until it reaches the edge of the grid or a blocked cell.
- *
- * Given a grid, a start and an end position, determine the number of moves it will take to get to the end position.
- *
- * For example, you are given a grid with sides n=3 described as follow:
- *
- * ...
- * .X.
- * ...
- *
- * Your starting position (startX, startY) = (0,0) so you start in the top left corner.
- *
- * The ending position is (goalX, goalY) = (1,2).
- *
- * The path is (0,0) -> (0,2) -> (1,2).
- *
- * It takes 2 moves to get to the goal.
- *
- * another (e.g)
- *
- * 3
- *
- * .X.
- * .X.
- * ...
- *
- * 0 0 0 2
- *
+ * @author neo82
  */
+@SuppressWarnings("Duplicates")
 public class CastleOnTheGrid {
-	static int n;
-
-	static int startY = 0;
-	static int startX = 0;
-
-	static int goalY = 0;
-	static int goalX = 0;
-
-	static YX goalYX;
-
-	static char [][] globalGrid;
-
-	static final char BLOCK = 'X';
-
-	static int cnt = 0;
-
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		n = scanner.nextInt();
+		int n = scanner.nextInt();
 
-		globalGrid = new char[n][n];
+		String [] grid = new String[n];
 
-		for (int y = 0; y < n; y++) {
-			String line = scanner.next();
-			for (int x = 0; x < n; x++) {
-				globalGrid[y][x] = line.charAt(x);
+		for (int i = 0; i < n; i++) {
+			grid[i] = scanner.next();
+		}
+
+		int startX = scanner.nextInt(); // vertical
+		int startY = scanner.nextInt(); // horizontal
+		int goalX = scanner.nextInt();
+		int goalY = scanner.nextInt();
+
+		System.out.println(minimumMoves(grid, startX, startY, goalX, goalY));
+	}
+
+	private static final char BLOCKED = 'X';
+
+	static int minimumMoves(String[] grid, int startX, int startY, int goalX, int goalY) {
+		int n = grid.length;
+
+		boolean [][] discovered = new boolean[n][n];
+
+		Queue<XY> q = new LinkedList<>();
+		q.add(new XY(startX, startY, 0));
+
+		discovered[startX][startY] = true;
+
+		while (q.isEmpty() == false) {
+			XY xy = q.poll();
+
+			int x = xy.x;
+			int y = xy.y;
+			int path = xy.path;
+
+			if (x == goalX && y == goalY) {
+				return xy.path;
+			}
+
+			// top
+			for (int nextX = x-1; nextX >= 0 && grid[nextX].charAt(y) != BLOCKED; nextX--) {
+				if (discovered[nextX][y] == false) {
+					q.add(new XY(nextX, y, path + 1));
+					discovered[nextX][y] = true;
+				}
+			}
+
+			// bottom
+			for (int nextX = x+1; nextX < n && grid[nextX].charAt(y) != BLOCKED; nextX++) {
+				if (discovered[nextX][y] == false) {
+					q.add(new XY(nextX, y, path + 1));
+					discovered[nextX][y] = true;
+				}
+			}
+
+			// left
+			for (int nextY = y-1; nextY >= 0 && grid[x].charAt(nextY) != BLOCKED; nextY--) {
+				if (discovered[x][nextY] == false) {
+					q.add(new XY(x, nextY, path + 1));
+					discovered[x][nextY] = true;
+				}
+			}
+
+			// right
+			for (int nextY = y+1; nextY < n && grid[x].charAt(nextY) != BLOCKED; nextY++) {
+				if (discovered[x][nextY] == false) {
+					q.add(new XY(x, nextY, path + 1));
+					discovered[x][nextY] = true;
+				}
 			}
 		}
 
-		startY = scanner.nextInt();
-		startX = scanner.nextInt();
-
-		goalY = scanner.nextInt();
-		goalX = scanner.nextInt();
-
-		goalYX = new YX(goalY, goalX);
-
-		System.out.println(solve(globalGrid, startY, startX, null));
-		System.out.println(cnt);
+		return -1;
 	}
 
-	enum Direction {
-		UP,
-		DOWN,
-		LEFT,
-		RIGHT;
-
-		int getAdd(Direction direction) {
-			return this == direction ? 0 : 1;
-		}
-	}
-
-	/**
-	 * 무조건 끝까지 가는게 아니고..
-	 *
-	 * 한칸만 앞으로 가고, 나머지는 옆으로 가는것이 최선의 방법일 수도 있다..
-	 *
-	 * 즉, 한칸마다 다음방향을 탐색하되, 방향을 바꾼다면 카운트가 +1 이 되고, 방향을 바꾸지 않는다면 카운트가 +0 이 된다.
-	 *
-	 * 이렇게 하면 중복 탐색이 너무 많아진다..
-	 *
-	 * 한번 방문한 좌표도 수십번 재방문이 가능하다.
-	 *
-	 * 예를 들어, 다음과 같은 예제는 끝나질 않는다.
-	 *
-	 * 10
-	 * .X..XX...X
-	 * X.........
-	 * .X.......X
-	 * ..........
-	 * ........X.
-	 * .X...XXX..
-	 * .....X..XX
-	 * .....X.X..
-	 * ..........
-	 * .....X..XX
-	 * 9 1 9 6
-	 *
-	 * @param grid
-	 * @param y
-	 * @param x
-	 * @return
-	 */
-	static int solve(char [][] grid, int y, int x, Direction direction) {
-		cnt++;
-
-		if (isGoal(y, x)) {
-			return 0;
-		}
-
-		int min = 987654321;
-
-		grid[y][x] = BLOCK;
-
-		// up
-		if (isValid(grid, y-1, x)) {
-			min = Math.min(min, solve(newGrid(grid), y-1, x, Direction.UP) + Direction.UP.getAdd(direction));
-		}
-
-		// down
-		if (isValid(grid, y+1, x)) {
-			min = Math.min(min, solve(newGrid(grid), y+1, x, Direction.DOWN) + Direction.DOWN.getAdd(direction));
-		}
-
-		// left
-		if (isValid(grid, y, x-1)) {
-			min = Math.min(min, solve(newGrid(grid), y, x-1, Direction.LEFT) + Direction.LEFT.getAdd(direction));
-		}
-
-		// right
-		if (isValid(grid, y, x+1)) {
-			min = Math.min(min, solve(newGrid(grid), y, x+1, Direction.RIGHT) + Direction.RIGHT.getAdd(direction));
-		}
-
-		return min;
-	}
-
-
-	static char[][] newGrid(char[][] grid) {
-		char [][] newGrid = new char[grid.length][];
-
-		for (int i = 0; i < grid.length; i++) {
-			newGrid[i] = Arrays.copyOf(grid[i], grid[i].length);
-		}
-
-		return newGrid;
-	}
-
-
-	static boolean isValid(char[][] grid, int y, int x) {
-		if (y < 0 || y >= n) {
-			return false;
-		} else if (x < 0 || x >= n) {
-			return false;
-		} else if (grid[y][x] == BLOCK) {
-			return false;
-		}
-
-		return true;
-	}
-
-	static boolean isGoal(int y, int x) {
-		return goalY == y && goalX == x;
-	}
-
-	static class YX {
-		int y;
+	static class XY {
 		int x;
+		int y;
+		int path;
 
-		YX(int y, int x) {
-			this.y = y;
+		XY(int x, int y, int path) {
 			this.x = x;
-		}
-
-		@Override
-		public boolean equals(Object o) {
-			if (this == o) {
-				return true;
-			}
-			if (o == null || getClass() != o.getClass()) {
-				return false;
-			}
-
-			YX yx = (YX)o;
-
-			if (y != yx.y) {
-				return false;
-			}
-			return x == yx.x;
-		}
-
-		@Override
-		public int hashCode() {
-			int result = y;
-			result = 31 * result + x;
-			return result;
+			this.y = y;
+			this.path = path;
 		}
 	}
 }
--- src/main/java/hackerrank/cs/dataStructure/stack/BalancedBrackets.java
+++ src/main/java/hackerrank/cs/dataStructure/stack/BalancedBrackets.java
@@ -3,53 +3,42 @@ package hackerrank.cs.dataStructure.stack;
 import java.util.Scanner;
 import java.util.Stack;
 
+/**
+ * @author neo82
+ */
 public class BalancedBrackets {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
 		int n = scanner.nextInt();
 
-		while (n-- > 0) {
-			String line = scanner.next();
-
-			solve(line);
+		for (int i = 0; i < n; i++) {
+			System.out.println(isBalanced(scanner.next()));
 		}
 	}
 
-	static void solve(String line) {
-		Stack<Character> st = new Stack<Character>();
+	static String isBalanced(String s) {
 
-		for (int i = 0; i < line.length(); i++) {
-			char ch = line.charAt(i);
+		Stack<Character> stack = new Stack<>();
 
-			if (ch == '{' || ch == '[' || ch == '(') {
-				st.push(ch);
-			} else if (st.isEmpty()) {
-				System.out.println("NO");
-				return;
-			} else if (isValid(st.pop(), ch) == false) {
-				System.out.println("NO");
-				return;
+		for (char ch : s.toCharArray()) {
+			if (isCloseBracket(ch) == false) {
+				stack.push(ch);
+			} else if (isCloseBracket(ch) && stack.isEmpty()) {
+				return "NO";
+			} else if (ch == ')' && stack.pop() != '('){
+				return "NO";
+			} else if (ch == '}' && stack.pop() != '{') {
+				return "NO";
+			} else if (ch == ']' && stack.pop() != '[') {
+				return "NO";
 			}
 		}
 
-		if (st.isEmpty() == false) {
-			System.out.println("NO");
-			return;
-		}
-
-		System.out.println("YES");
+		return stack.isEmpty() ? "YES" : "NO";
 	}
 
-	static boolean isValid(char open, char close) {
-		if (open != '(' && close == ')') {
-			return false;
-		} else if (open != '[' && close == ']') {
-			return false;
-		} else if (open != '{' && close == '}') {
-			return false;
-		}
-
-		return true;
+	static boolean isCloseBracket(char ch) {
+		return ch == ')' || ch == '}' || ch == ']';
 	}
 }
