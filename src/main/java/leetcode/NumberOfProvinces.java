6df57cdbb8f09c5128c9e429467380b91dba944f
neo82 <baejunbeom82@gmail.com>
2021-03-18 19:35:45 +0900

    Number Of Provinces

--- src/main/java/leetcode/NumberOfProvinces.java
+++ src/main/java/leetcode/NumberOfProvinces.java
@@ -1,48 +1,76 @@
 package leetcode;
 
-import java.util.*;
-
 public class NumberOfProvinces {
-	public int findCircleNum(int[][] M) {
-		int n = M.length;
-		int count = 0;
-		boolean [] visited = new boolean[n];
+	static class UnionFind {
+		int [] parent;
+		int [] rank;
+		int count;
 
-		for (int y = 0; y < n; y++) {
-			if (visited[y] == false) {
-				Queue<Integer> q = new LinkedList<>();
-				q.add(y);
+		UnionFind(int n) {
+			this.parent = new int[n];
+			this.rank = new int[n];
+			this.count = n;
 
-				while (q.isEmpty() == false) {
-					int size = Integer.valueOf(q.size());
-
-					for (int i = 0; i < size; i++) {
-						int hereY = q.poll();
-
-						for (int x = hereY; x < n; x++) {
-							if (isValid(hereY, x, M, visited)) {
-								visited[x] = true;
-								q.add(x);
-							}
-						}
-					}
-				}
-
-				count++;
+			for (int i = 0; i < n; i++) {
+				parent[i] = i;
 			}
 		}
 
-		return count;
-	}
+		public int find(int u) {
+			if (u != parent[u]) {
+				parent[u] = find(parent[u]);
+			}
 
-	private boolean isValid(int y, int x, int[][] M, boolean[] visited) {
-		if (y < 0 || y >= M.length || x < 0 || x >= M.length) {
-			return false;
-		} else if (visited[x]) {
-			return false;
+			return parent[u];
 		}
 
-		return M[y][x] == 1;
+		public void union(int u, int v) {
+			int uRoot = find(u);
+			int vRoot = find(v);
+
+			if (uRoot == vRoot) {
+				return;
+			}
+
+			if (rank[uRoot] < rank[vRoot]) {
+				parent[uRoot] = vRoot;
+			} else if (rank[uRoot] > rank[vRoot]) {
+				parent[vRoot] = uRoot;
+			} else {
+				parent[uRoot] = vRoot;
+				rank[vRoot]++;
+			}
+
+			// union 이 안된 상태로 그대로 남아있는 아이도 있기 때문에 0 에서 ++ 를 하면 안되고
+			// 전체 노드의 수에서 -- 를 해주어야 한다.
+			count--;
+		}
+
+		int getCount() {
+			return this.count;
+		}
+
 	}
 
+	public int findCircleNum(int[][] M) {
+		UnionFind unionFind = new UnionFind(M.length);
+
+		for (int i = 0; i < M.length; i++) {
+			for (int j = 0; j < M.length; j++) {
+				if (M[i][j] == 1) {
+					unionFind.union(i, j);
+				}
+			}
+		}
+
+		return unionFind.getCount();
+	}
+
+	public static void main(String[] args) {
+		int [][] M = {{1,1,0},{1,1,0},{0,0,1}};
+
+		NumberOfProvinces numberOfProvinces = new NumberOfProvinces();
+
+		System.out.println(numberOfProvinces.findCircleNum(M));
+	}
 }
